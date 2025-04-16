21499bd287f2309d3d0892248df3a8932ab0539d
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-04-16 20:50:26 +0100

    style: format code for consistency and readability

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config();
 // Connect to MongoDB
 connectDB();
 
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(cors({
-    origin: 'http://localhost:5173', // frontend origin
-    credentials: true,               // if using cookies/auth headers
-  }));
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend origin
+    credentials: true, // if using cookies/auth headers
+  })
+);
 
-app.get('/', (_req, res) => {
-  res.send('Hello, TypeScript with Express!');
+app.get("/", (_req, res) => {
+  res.send("Hello, TypeScript with Express!");
 });
 
 app.listen(PORT, () => {
