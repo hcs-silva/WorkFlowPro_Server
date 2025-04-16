155e67f533af40400c4ee49d81d3a2fb668284a6
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-04-16 20:13:05 +0100

    feat: initialize TypeScript Express server with MongoDB connection
    - Added package.json with dependencies and scripts for development and production.
    - Implemented MongoDB connection in src/config/db.ts.
    - Set up Express server in src/index.ts with middleware for logging and CORS.
    - Configured TypeScript compiler options in tsconfig.json.

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import cors from 'cors';
+import morgan from 'morgan';
+
+dotenv.config();
+// Connect to MongoDB
+connectDB();
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(cors({
+    origin: 'http://localhost:5173', // frontend origin
+    credentials: true,               // if using cookies/auth headers
+  }));
+
+app.get('/', (_req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
